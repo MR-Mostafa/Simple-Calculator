daee5a5b40534a0db3fc11640a3f9d831dc3896b
Mostafa Rahmati <m.rahmati70@hotmail.com>
2019-04-21 04:57:37 +0430

    calculate percentage

--- script/main.js
+++ script/main.js
@@ -11,56 +11,66 @@
     
 
 
-    function btn_number(e){
-        var event = e.target;
-        var logDisplay = 0;
-        
-        if(event.classList.contains('numbers')){
-            if(LED_display.textContent == "0"){
-                LED_display.textContent = '';
-            }
-            LED_display.textContent += event.textContent;
+function btn_number(e){
+    var event = e.target;
+    var logDisplay = 0;
+
+    if(event.classList.contains('numbers')){
+        if(LED_display.textContent == "0"){
+            LED_display.textContent = '';
+        }
+        LED_display.textContent += event.textContent;
+    }
+
+    if(event.classList.contains('operator')){
+        if(operator.test(LED_display.textContent[LED_display.textContent.length - 1])){
+            LED_display.textContent = LED_display.textContent.slice(0, LED_display.textContent.length-1);
         }
 
-        if(event.classList.contains('operator')){
-            if(operator.test(LED_display.textContent[LED_display.textContent.length - 1])){
-                LED_display.textContent = LED_display.textContent.slice(0, LED_display.textContent.length-1);
-            }
-
-            if(event.id != 'percentage' && secendOperand == true){
-                logDisplay = LED_display.textContent;
-                LED_display.textContent = eval(LED_display.textContent);
-            }
-            secendOperand = true;
-
-            LED_display.textContent += event.textContent;
-
-            if(event.id == 'percentage'){
-                var numSplit;
-                numSplit = LED_display.textContent.split(/[%]/);
-                logDisplay = numSplit[0] + '%';
-            }
-    
-            
-            
+        if(event.id != 'percentage' && secendOperand == true){
+            logDisplay = LED_display.textContent + '=';
+            LED_display.textContent = eval(LED_display.textContent);
         }
-    
-        if(logDisplay != 0){
-            log.textContent = logDisplay + "=";
-        }
-        
-        LED_displayVal = LED_display.textContent;
-        if(event.id == 'equal'){
-            if(/[%]/.test(LED_displayVal)){
-                console.log('%%%%%%');
+        secendOperand = true;
+
+        LED_display.textContent += event.textContent;
+
+        if(event.id == 'percentage'){
+            var numSplit, numSplitSecond, result, whichOperand;
+            numSplit = LED_display.textContent.split(/[%]/);
+            numSplitSecond = LED_display.textContent.split(/[+*\-\/%]/);
+           
+            if (/[+]/.test(numSplit[0])) whichOperand = '+';
+            if (/[-]/.test(numSplit[0])) whichOperand = '-';
+            if (/[/]/.test(numSplit[0])) whichOperand = '/';
+            if (/[*]/.test(numSplit[0])) whichOperand = '*';
+
+            if(whichOperand == '/'){
+                result = ((Number(numSplitSecond[1])/100));
             }else{
-                LED_display.textContent = eval(LED_display.textContent);
+                result = ((Number(numSplitSecond[1])/100))*Number(numSplitSecond[0]);
             }
-            
+
+            logDisplay = numSplit[0] + '%=' + result;
+            LED_display.textContent = eval(numSplitSecond[0] +  whichOperand + result);
         }
-                
+
+        
         
     }
+
+
+    if(logDisplay != 0){
+        log.textContent = logDisplay;
+    }
+    
+    LED_displayVal = LED_display.textContent;
+    if(event.id == 'equal'){
+        LED_display.textContent = eval(LED_display.textContent);
+    }
+            
+    
+}
 
 
     calculator.addEventListener('click', function(e){
