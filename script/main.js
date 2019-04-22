e4c71430ba70b506cfd1ee0a3b77248d636cffa0
Mostafa Rahmati <m.rahmati70@hotmail.com>
2019-04-22 15:38:40 +0430

    fuck percentage :angry:

--- script/main.js
+++ script/main.js
@@ -1,155 +1,123 @@
 (function(){
 
-    var calculator, log, LED_display, mainOperatorSign;
+    var calculator, log, display;
     calculator = window.calculator;
-    LED_display = calculator.querySelector('#LED-display');
+    display = calculator.querySelector('#LED-display');
     log = window.log;
-    mainOperatorSign = /[+-/*]/;
-    //number = /[0-9]/;
 
     var cal = {
-        displayValue        : "0", 
-        logDisplay          : "0",
-        result              : "0",
-        splitWithOperator   : function(){ return this.displayValue.split(/[+*\-\/%]/); },
-        splitWithPercentage : function(){ return this.displayValue.split(/[%]/); },
-        whichOperator       : function(){
-                                    if (/[+]/.test(this.splitWithPercentage()[0])) return '+';
-                                    if (/[-]/.test(this.splitWithPercentage()[0])) return '-';
-                                    if (/[/]/.test(this.splitWithPercentage()[0])) return '/';
-                                    if (/[*]/.test(this.splitWithPercentage()[0])) return '*';
-                                    return false;
-                                }
-    } 
+        displayValue     : "0",
+        result           : false,
+        resultPersent    : null,
+        numberOfDecimal  : 5,
+        getLastCharacter : function(){
+                return cal.displayValue[cal.displayValue.length-1];
+            },
+        splitWithOperator : function(){
+                // filter(Boolean) remove falsy items in arr
+                var arr = this.displayValue.split(/[-+*/%]/).filter(Boolean);
+                return arr;
+            },
+        whichOperator : function(){
+                if (/[+]/.test(this.displayValue)) return '+';
+                if (/[-]/.test(this.displayValue)) return '-';
+                if (/[/]/.test(this.displayValue)) return '/';
+                if (/[*]/.test(this.displayValue)) return '*';
+                return false;
+            }
+    }
     
 
-function updateDisplayValue(){
-    LED_display.textContent = cal.displayValue;
-    LED_display.textContent.toFixed(4);
+function update(){
+    display.textContent = cal.displayValue;
 }
-function showLog(){
-    log.textContent = cal.logDisplay;
-}
-function showResult(percentagebtn = false){
-    if(percentagebtn == true){
-        if(cal.result % 1 == 0){
-            log.textContent = cal.splitWithPercentage()[0] + '%=' + cal.result;
-            LED_display.textContent = eval(cal.splitWithOperator()[0] +  cal.whichOperator() + cal.result);
-        }else{
-            log.textContent = cal.splitWithPercentage()[0] + '%=' + +cal.result.toFixed(4);
-            LED_display.textContent = +eval(cal.splitWithOperator()[0] +  cal.whichOperator() + cal.result).toFixed(4);
-        }
-    }else{
-        if(eval(cal.displayValue) % 1 == 0){
-            log.textContent = cal.displayValue + '=' + eval(cal.displayValue);
-            LED_display.textContent = eval(cal.displayValue);
-        }else{
-            log.textContent = cal.displayValue + '=' + +eval(cal.displayValue).toFixed(4);
-            LED_display.textContent = +eval(cal.displayValue).toFixed(4);
-        }
+function showResult(){
+    if( cal.splitWithOperator().length >= 2 && /[-+*/%]/.test(cal.getLastCharacter()) == false ){
+        // Calculates value
+        cal.result = eval(cal.displayValue);
+        // show log
+        log.textContent = cal.displayValue + '=';
+        // reset cal.displayValue
+        cal.displayValue = cal.result;
+        //show cal.result
+        // the addition sign before the variable name, is deleted extra zeros in decimal number
+        // Ex: 12/5 before adding a plus sign is equal to 2.40000 BUT after adding a plus sign is equal 2.4
+        display.textContent = +cal.result.toFixed(cal.numberOfDecimal);
     }
 }
 
 
 function btn_number(e){
     var event = e.target;
+       
     if(event.classList.contains('numbers')){
-        if(cal.displayValue == "0"){
-            cal.displayValue = '';
-        }
+
+        // When the number buttons is clicked, the default value change. (Zero number is deleted)
+        if(cal.displayValue == "0") cal.displayValue = '';
+        
+        // When the number buttons is clicked, In the display is shown
         cal.displayValue += event.textContent;
-        updateDisplayValue();
-    }
+        update();
+        
+    } // if number
 
 
     if(event.classList.contains('operator')){
-        // If you click on the operator button again, it will remove the last operator in cal.displayValue
-        // Ex: if you've written 3+ and then click on minus button, the cal.displayValue is chacge to 3- (NOT 3+-)
-        if(mainOperatorSign.test(cal.displayValue[cal.displayValue.length - 1])){
-            cal.displayValue = cal.displayValue.slice(0, cal.displayValue.length-1);
+        // If you click on the operator buttons again, it will replace the last operator in cal.displayValue
+        // Ex: if you've written 3+ and then click on minus or division button, the cal.displayValue is chacge to 3- (NOT 3+-)
+        if(/[-+*/%]/.test(cal.getLastCharacter())){
+           cal.displayValue = cal.displayValue.slice(0, cal.displayValue.length-1);
         }
 
-        // if event.id is not equal to 'percentage' AND cal.secendOperand.length greater than or equal 2, then Calculates the value.
-        // Ex: if you've written 3+2 and then click on other operator buttons, it will show 5.
-        // It also updates cal.logDisplay
-        if(event.id != 'percentage'){
-            cal.logDisplay = cal.displayValue + '=';
-            cal.displayValue = eval(cal.displayValue);
-            updateDisplayValue();
-        }
-
-        // Write operator sign in cal.displayValue, if event.id not equal to 'percentage'
-        if(event.id != 'percentage') cal.displayValue += event.textContent; updateDisplayValue();
-
-        // Write 'percentage' sign in cal.displayValue, if event.id equal to 'percentage'
-        // you can't Write 'percentage' sign as a first Operand
-        console.log(cal.splitWithOperator());
         if(event.id == 'percentage' && cal.splitWithOperator().length >= 2){
             
             cal.displayValue += event.textContent;
-            updateDisplayValue();
+            update();
 
             if(cal.whichOperator() == '/'){
-                cal.result = Number(cal.splitWithOperator()[1])/100;
+                cal.resultPersent = Number(cal.splitWithOperator()[1])/100;
             }else{
-                cal.result = (Number(cal.splitWithOperator()[1])/100)*Number(cal.splitWithOperator()[0]);
+                cal.resultPersent = (Number(cal.splitWithOperator()[1])/100)*Number(cal.splitWithOperator()[0]);
             }
-            showResult(true);
+            console.log(cal.splitWithOperator());
+            // show log
+            log.textContent = cal.displayValue + '=' + +cal.resultPersent.toFixed(cal.numberOfDecimal);
+
+            if(cal.whichOperator() == '/') cal.result = Number(cal.splitWithOperator()[0]) / Number(cal.resultPersent);
+            if(cal.whichOperator() == '*') cal.result = Number(cal.splitWithOperator()[0]) * Number(cal.resultPersent);
+            if(cal.whichOperator() == '+') cal.result = Number(cal.splitWithOperator()[0]) + Number(cal.resultPersent);
+            if(cal.whichOperator() == '-') cal.result = Number(cal.splitWithOperator()[0]) - Number(cal.resultPersent);
+            
+            //console.log(cal.whichOperator());
+            //console.log(cal.splitWithOperator());
+            // reset cal.displayValue
+            cal.displayValue = cal.result;
+            //show cal.result
+            // the addition sign before the variable name, is deleted extra zeros in decimal number
+            // Ex: 12/5 before adding a plus sign is equal to 2.40000 BUT after adding a plus sign is equal 2.4
+            display.textContent = +cal.result.toFixed(cal.numberOfDecimal);
+            
         }
         
-
-        /*
-        if(event.id == 'percentage'){
-            var numSplit, numSplitSecond, result, whichOperand;
-            numSplit = LED_display.textContent.split(/[%]/);
-            numSplitSecond = LED_display.textContent.split(/[+*\-\/%]/);
-            if(numSplitSecond.length >= 2){
-                LED_display.textContent += event.textContent;
-                if (/[+]/.test(numSplit[0])) whichOperand = '+';
-                if (/[-]/.test(numSplit[0])) whichOperand = '-';
-                if (/[/]/.test(numSplit[0])) whichOperand = '/';
-                if (/[*]/.test(numSplit[0])) whichOperand = '*';
-
-                if(whichOperand == '/'){
-                    result = ((Number(numSplitSecond[1])/100));
-                }else{
-                    result = ((Number(numSplitSecond[1])/100))*Number(numSplitSecond[0]);
-                }
-
-                if(result % 1 == 0){
-                    logDisplay = numSplit[0] + '%=' + result;
-                    LED_display.textContent = eval(numSplitSecond[0] +  whichOperand + result);
-                }else{
-                    logDisplay = numSplit[0] + '%=' + +result.toFixed(4);
-                    LED_display.textContent = +eval(numSplitSecond[0] +  whichOperand + result).toFixed(4);
-                }
-                
+        
+        if(event.id != 'percentage'){
+            // if cal.splitWithOperator.length greater than or equal 2, then calculates that value and show it.
+            // Ex: if you've written 3+2 and then click on other operator buttons, it will show 5.
+            if(cal.splitWithOperator().length >= 2){
+                showResult();
             }
-        }*/
+            // update cal.displayValue
+            cal.displayValue += event.textContent;
+            update();
+        }
+       
 
-        
-        
-    }
+    } // if operator
 
     if(event.id == 'equal'){
-
-        showResult(false);
-    }
-
-    /*if(logDisplay != 0){
-        log.textContent = logDisplay;
-    }
+        showResult();
+    } // if equal
     
-    LED_displayVal = LED_display.textContent;
-    if(event.id == 'equal'){
-        var result = eval(LED_display.textContent);
-        if(result % 1 == 0){
-            LED_display.textContent = +result;
-        }else{
-            LED_display.textContent = +result.toFixed(4);
-        }
-    }*/
-           
     
 }
 
